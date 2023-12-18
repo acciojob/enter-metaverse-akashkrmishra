88f41cf6644734c398403a4f928fdabc93366955
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2023-12-18 21:11:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const btn = document.getElementById("enterbtn");
+
+const f1 = ()=> {
+	console.log("Entered Metaverse");
+}
+
+btn.addEventListener("click",f1);
+
+function x() {
+	btn.removeEventListener("click",f1);
+}
