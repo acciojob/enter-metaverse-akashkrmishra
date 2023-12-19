cceab2a07c8618bedff8d51568e0d055d35129c6
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2023-12-19 18:38:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-const btn = document.querySelector("#enterbtn");
+const btn = document.querySelector("enterbtn");
 btn.addEventListener("click",() => {
 	document.getElementById('status').innerHTML = '<h1>Entered Metaverse</h1>';
 });
